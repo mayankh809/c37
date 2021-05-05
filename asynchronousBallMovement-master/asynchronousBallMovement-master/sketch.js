d56933e09484a177e65a2bf50aebb76ea107e673
mayankh809 <76521097+mayankh809@users.noreply.github.com>
2021-05-05 17:57:47 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,26 @@
+var game,player,form
+var gameState=0
+var playerCount
+var database
+var allPlayers
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    game=new Game()
+    game.getState()
+    game.start()
+
+    
+}
+
+function draw(){
+    background("white");
+   if(playerCount===4){
+       game.update(1)
+   }
+   if(gameState===1){
+       clear()
+       game.play()
+   }
+}
+
